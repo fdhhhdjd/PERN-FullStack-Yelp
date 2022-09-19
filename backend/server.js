3494db023f94a4e6862d223cc934442bedd0fe93
tai <nguyentientai10@gmail.com>
2022-09-19 16:23:05 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,12 @@ app.get("/", (req, res) => {
   };
   return res.send(healthcheck);
 });
-app.use(express.static(path.join(__dirname, "./frontend/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./frontend/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`server is listening on port:http://localhost:${PORT}`)
